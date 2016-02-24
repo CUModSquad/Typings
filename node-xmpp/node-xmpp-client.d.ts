cf4d6dceee2c73df65b2977d2db8ecb1bac9918a
JB <jb@codecorsair.com>
2016-02-24 11:50:17 -0500

    Remove Element from node-xmpp-client

--- node-xmpp/node-xmpp-client.d.ts
+++ node-xmpp/node-xmpp-client.d.ts
@@ -18,19 +18,6 @@ declare module xmpp  {
     send(elem: any): void;
     disconnect(reason: string): void;
   }
-
-  class Element{
-    is: any;
-    attrs: any;
-    from: string;
-    
-    constructor (t: any, attrs?: any);
-    c(t:string):Element;
-    c(t:string, d:any):Element;
-    t(message:string):Element;
-    getChild(t:string):any;
-    getText():string;
-  }
 }
 
 declare module 'node-xmpp-client' {
