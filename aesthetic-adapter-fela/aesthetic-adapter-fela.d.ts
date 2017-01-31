ca6645436e188771adb9f535de65466cdff8e612
JB <jb@codecorsair.com>
2017-01-31 11:52:17 -0500

    basic aesthetic-adapter-fela

---
+++ aesthetic-adapter-fela/aesthetic-adapter-fela.d.ts
@@ -0,0 +1,9 @@
+// Type definitions for aesthetic-adapter-fela 0.5.0
+// Project: https://github.com/milesj/aesthetic/tree/master/packages/aesthetic-adapter-fela
+// Definitions by: JB (codecorsair) <https://github.com/codecorsair>
+
+declare module 'aesthetic-adapter-fela' {
+  export default class FelaAdapter {
+    constructor();
+  }
+}
