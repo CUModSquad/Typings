7321510e0f01c11354727960d67c2bcf9db6c742
JB <jb@codecorsair.com>
2017-01-31 12:14:56 -0500

    update options on aestetic typing

--- aesthetic/aesthetic.d.ts
+++ aesthetic/aesthetic.d.ts
@@ -4,18 +4,17 @@
 
 declare module 'aesthetic' {
   export default class Aesthetic {
-    constructor(adapter: any);
+    constructor(adapter: any, options?: Partial<AestheticOptions>);
   }
 
+  interface AestheticOptions {
+    styleName: string; // The unique style name of the component. This name is primarily used in logging and caching. Defaults to the component or function name.
+    extendable: boolean; // Allows the component and its styles to be extended, creating a new component in the process. Defaults to false.
+    stylesPropName: string; // Name of the prop in which the compiled class names or styles object is passed to. Defaults to 'className'.
+    themePropName: string; // Name of the prop in which the theme name is passed to. Defaults to 'theme'.
+  }
 
-
-  export function createStyler(aesthetic: Aesthetic): (style: any, 
-    options: Partial<{  
-      styleName: string; // The unique style name of the component. This name is primarily used in logging and caching. Defaults to the component or function name.
-      extendable: boolean; // Allows the component and its styles to be extended, creating a new component in the process. Defaults to false.
-      stylesPropName: string; // Name of the prop in which the compiled class names or styles object is passed to. Defaults to 'className'.
-      themePropName: string; // Name of the prop in which the theme name is passed to. Defaults to 'theme'.
-    }>) => (element: any) => any;
+  export function createStyler(aesthetic: Aesthetic): (style: any, options: Partial<AestheticOptions>) => (element: any) => any;
 
   export function classes(...params: (string | any)[]): string;
 
