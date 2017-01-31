086a794e369d083ba00de21d264cb59d385e128e
JB <jb@codecorsair.com>
2017-01-31 12:03:38 -0500

    basic fela-preset-web

---
+++ fela-preset-web/fela-preset-web.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for fela-preset-web 4.2.0
+// Project: https://github.com/rofrischmann/fela/tree/master/packages/fela-preset-web
+// Definitions by: JB (codecorsair) <https://github.com/codecorsair>
+
+declare module 'fela-preset-web' {
+  const webPreset: any;
+  export default webPreset;
+}
