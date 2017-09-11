726a19dbe14a4dfb96e180715b02d3b74ac967e7
Justin Vick <justin.zezmin@gmail.com>
2017-09-11 16:14:12 -0500

    pan zoom element added

---
+++ react-pan-zoom-elements/react-pan-zoom-element.d.ts
@@ -0,0 +1,11 @@
+declare module 'react-pan-zoom-element' {
+    class PanZoomElement extends React.Component<PanZoomProps, any>{
+
+    }
+    interface PanZoomProps {
+        width: number,
+        height: number,
+    }
+
+    export default PanZoomElement;
+}
