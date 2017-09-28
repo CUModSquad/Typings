51cfea8e3358b70426e08d117db28c3cee153748
Ajackster <jacksonal300@gmail.com>
2017-09-28 15:27:41 -0400

    Add introjs-react typings

---
+++ intro.js-react/introjs-react.d.ts
@@ -0,0 +1,44 @@
+declare module 'intro.js-react' {
+  // Steps component
+  export class Steps extends React.Component<StepsProps, {}> {
+  }
+
+  export interface StepInfo {
+    element: string;
+    intro: string;
+    position?: string;
+    tooltipClass?: string;
+    highlightClass?: string;
+  }
+
+  export interface StepsProps {
+    enabled?: boolean;
+    initialStep: number;
+    onExit?: (index: number) => void;
+    onStart?: (index: number) => void;
+    onChange?: (nextIndex: number, nextElement: string) => void;
+    onBeforeChange?: (nextIndex: number) => void;
+    onAfterChange?: (index: number, element: string) => void;
+    onPreventChange?: (index: number) => void;
+    onComplete?: () => void;
+    options?: any;
+  }
+
+  // Hints component
+  export class Hints extends React.Component<HintsProps, {}> {
+  }
+
+  export interface HintInfo {
+    element: string;
+    hint: string;
+    hintPosition?: string;
+  }
+
+  export interface HintsProps {
+    enabled?: boolean;
+    hints: HintInfo[];
+    onClick?: () => void;
+    onClose?: () => void;
+    options: any;
+  }
+}
