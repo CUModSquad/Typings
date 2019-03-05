2646aae4100b88830d24a95f5d017b71c3548ac4
JB <jb@codecorsair.com>
2019-03-05 17:29:31 -0500

    Add initial typings for velocity-react

---
+++ velocity-react/velocity-react.d.ts
@@ -0,0 +1,23 @@
+declare module 'velocity-react' {
+
+  export type VelocityComponentProps = {
+    children: React.ReactElement<any>;
+    animation?: any; // default null
+    runOnMount?: boolean; // default false
+    targetQuerySelector?: string; // default null
+    interruptBehavior?: string; // default 'stop'
+  } & Partial<jquery.velocity.Options>;
+  export type VelocityComponet = React.Component<VelocityComponentProps, {}>;
+
+  export type VelocityTransitionGroupProps = {
+    runOnMount?: boolean;
+    enter?: any;
+    leave?: any;
+    children?: any;
+    enterHideStyle?: object;
+    enterShowStyle?: object;
+  }
+  export type VelocityTransitionGroup = React.Component<VelocityTransitionGroupProps, {}>;
+
+
+}
